baa8d381eb554ba3eaadaa973f3ce0c530f5be8f
Sigi683 <adrien.malgouyres@outlook.fr>
2026-03-23 22:15:18 +0100

    feat(main): Added a main file to test tokenizer

--- src/tokenizer/main.py
+++ src/tokenizer/main.py
@@ -1,5 +1,3 @@
-with open("the-verdict.txt", "r", encoding="utf-8") as f:
-    raw_text = f.read()
-    
-print("Total number of character:", len(raw_text))
-print(raw_text[:99])
+from tokenizer import tokenize
+
+tokenize()
