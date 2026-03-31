c714523d32558d8d9ddc22063cb70cb2f0468ad7
Sigi683 <adrien.malgouyres@outlook.fr>
2026-03-31 15:44:08 +0200

    fix(tokenizer): Fixed Invalid key with empty space

--- src/tokenizer/tokenizer.py
+++ src/tokenizer/tokenizer.py
@@ -9,30 +9,22 @@ class Tokenizer:
 
     def preprocess(self, raw_text):
         split = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
-        preprocessed = []
-        for item in split:
-            preprocessed.append(item.strip())
-        return preprocessed
+        return [item.strip() for item in split if item.strip()]
 
     def create_vocabulary(self, file):
         with open("the-verdict.txt", "r", encoding="utf-8") as f:
             raw_text = f.read()
-        
+
         preprocessed = self.preprocess(raw_text)
 
-        vocab = {  }
-        index = 0
+        unique_tokens = sorted(list(set(preprocessed)))
 
-        for c in raw_text.split() :
-            if (not c in vocab.values()):
-                vocab[index] = c
-                index+=1
-
+        vocab = {token: i for i, token in enumerate(unique_tokens)}
         return vocab
 
-        def encode(self, text):
-            preprocessed = self.preprocess(text)
-            encoded = []
-            for s in preprocessed:
-                encoded.append(self.str_to_int[s])
-            return encoded
+    def encode(self, text):
+        preprocessed = self.preprocess(text)
+        encoded = []
+        for s in preprocessed:
+            encoded.append(self.str_to_int[s])
+        return encoded
