15f9b83a532383f1c0912c27db89f160abccce16
Sigi683 <adrien.malgouyres@outlook.fr>
2026-04-28 17:45:11 +0200

    Cleaned repo

--- src/tokenizer/main.py
+++ src/tokenizer/main.py
@@ -1,3 +1,11 @@
-from tokenizer import tokenize
+from tokenizer import Tokenizer
 
-tokenize()
+t = Tokenizer("the-verdict.txt")
+
+text = """"It's the last he painted, you know," 
+           Mrs. Gisburn said with pardonable pride."""
+
+ids = t.encode(text)
+print(len(ids))
+print(ids == [1, 56, 2, 850, 988, 602, 533, 746, 5, 1126, 596, 5, 1, 67, 7, 38, 851, 1108, 754, 793, 7])
+print(t.decode(ids) == " It' s the last he painted, you know,\" Mrs. Gisburn said with pardonable pride.")
